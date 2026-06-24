949adf14413dd450dca3fe4110ca83703730e324
TinchoToro <116118326+TinchoToro@users.noreply.github.com>
2026-06-24 07:45:11 -0300

    Add eliminarAlumno route to docente.js

--- backend/routes/docente.js
+++ backend/routes/docente.js
@@ -1,6 +1,6 @@
 // routes/docente.js
 const router = require('express').Router();
-const { listarAlumnos, detalleAlumno, resumenEscuela } = require('../controllers/docenteController');
+const { listarAlumnos, detalleAlumno, resumenEscuela, eliminarAlumno } = require('../controllers/docenteController');
 const { verificarToken, soloRol } = require('../middleware/auth');
 
 const esDocente = [verificarToken, soloRol('docente', 'admin')];
@@ -8,5 +8,6 @@ const esDocente = [verificarToken, soloRol('docente', 'admin')];
 router.get('/resumen',        ...esDocente, resumenEscuela);
 router.get('/alumnos',        ...esDocente, listarAlumnos);
 router.get('/alumnos/:id',    ...esDocente, detalleAlumno);
+router.delete('/alumnos/:id', ...esDocente, eliminarAlumno);
 
 module.exports = router;
